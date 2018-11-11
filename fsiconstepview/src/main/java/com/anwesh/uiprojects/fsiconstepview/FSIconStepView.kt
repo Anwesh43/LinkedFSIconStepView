f0c559f8cca172a597cf115780d93c04005a1036
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 09:31:08 +0530

    importing libraries and defining nodes

--- fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
+++ fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.fsiconstepview
 /**
  * Created by anweshmishra on 11/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+
+val lines : Int = 4
+
+val scGap : Float = 0.05f
+
