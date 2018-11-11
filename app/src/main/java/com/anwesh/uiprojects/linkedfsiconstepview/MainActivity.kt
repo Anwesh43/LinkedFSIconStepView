31bb325148ed7841c84a50b3f6ee9240a3356492
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 10:19:33 +0530

    using view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedfsiconstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfsiconstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedfsiconstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.fsiconstepview.FSIconStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        FSIconStepView.create(this)
     }
 }
