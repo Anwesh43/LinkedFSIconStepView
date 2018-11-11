25fc01a04633bc3c40ed680bee0bd40464f0830b
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 10:56:10 +0530

    adding OnAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedfsiconstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedfsiconstepview/MainActivity.kt
@@ -3,14 +3,24 @@ package com.anwesh.uiprojects.linkedfsiconstepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.fsiconstepview.FSIconStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FSIconStepView.create(this)
+        val view : FSIconStepView = FSIconStepView.create(this)
         fullScreen()
+        view.addOnAnimationListener({
+            createToast("animation number $it is complete")
+        }, {
+            createToast("animation number $it is reset")
+        })
+    }
+
+    private fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
