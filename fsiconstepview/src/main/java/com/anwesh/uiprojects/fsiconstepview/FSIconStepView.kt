be51344d45b52d683fec70cb763c7e77f210a6da
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 10:12:39 +0530

    adding FSIconStep class

--- fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
+++ fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
@@ -186,4 +186,27 @@ class FSIconStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FSIconStep(var i : Int) {
+        private var curr : FSISNode = FSISNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
