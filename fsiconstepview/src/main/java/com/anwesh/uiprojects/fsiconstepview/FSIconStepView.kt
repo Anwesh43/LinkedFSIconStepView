5ff394f36a8205b3e32c10208f77d946e145f0f4
Anwesh43 <anweshthecool0@gmail.com>
2018-11-11 10:10:13 +0530

    adding FSISNode class

--- fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
+++ fsiconstepview/src/main/java/com/anwesh/uiprojects/fsiconstepview/FSIconStepView.kt
@@ -141,4 +141,49 @@ class FSIconStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class FSISNode(var i : Int, val state : State = State()) {
+
+        private var next : FSISNode? = null
+
+        private var prev : FSISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = FSISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawFSISNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : FSISNode {
+            var curr : FSISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
